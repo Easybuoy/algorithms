18e205ef3a87ed25d53596c34f56c533825cec7d
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-02 09:09:34 +0100

    refactor twostrings algorithm

--- twoStrings.py
+++ twoStrings.py
@@ -1,14 +1,10 @@
 def twoStrings(s1, s2):
     isMatch = 'NO'
-    if(not s1 and not s2):
-        return isMatch
-
     string1 = list(s1)
-    string2 = list(s2)
     for elem in range(len(string1)):
-        for elem2 in range(len(string2)):
-            if (string1[elem] == string2[elem2]):
-                isMatch = 'YES'
+        print(s2.find(s1[elem]))
+        if (s2.find(s1[elem]) != -1):
+            isMatch = 'YES'
 
     return isMatch
 
