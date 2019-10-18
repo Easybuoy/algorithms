04cfd6d78b2a7d8f7a88a50c5f9424833e16cbe7
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-18 22:27:21 +0100

    solve FlightMovie

---
+++ FlightMovie.js
@@ -0,0 +1,19 @@
+calculateFlightMovie = (flightTime, moviesList) => {
+  let movieObj = {};
+  isValidMovie = false;
+  moviesList.forEach((movie, index) => {
+    let diff = flightTime - movie;
+
+    if (diff in movieObj && index != movieObj[movie]) {
+      isValidMovie = true;
+    }
+    movieObj[movie] = index;
+  });
+
+  return isValidMovie;
+};
+
+console.log(calculateMovie(180, [120, 20, 40, 70]));
+// console.log(calculateMovie(160, [80, 20, 40, 80]));
+console.log(calculateMovie(195, [75, 120, 65, 140, 80, 95, 45, 72]));
+// {75: 0, 140: 1, 120: 2, 80: 3}
