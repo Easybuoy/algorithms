03ff41a69a47e7b7e3e55064e64e177feea4db84
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-12-11 18:58:47 +0100

    solve fibonacci_sequence while loop

--- fibonacci_sequence.js
+++ fibonacci_sequence.js
@@ -1,20 +1,50 @@
-const fibonacci_sequence = (sequence, fibonacci_response = [0, 1]) => {
+// const fibonacci_sequence_recursive = (sequence, fibonacci_response = [0, 1]) => {
+//     if (sequence === 0) {
+//         return []
+//     }
+    
+//     if (sequence == 1) {
+//         return [0]
+//     }
+
+//     if (sequence === 2) {
+//         return fibonacci_response;
+//     }
+
+//     if (sequence === fibonacci_response.length) {
+//         return fibonacci_response;
+//     }
+
+//     const next_number = fibonacci_response[fibonacci_response.length - 1]  + fibonacci_response[fibonacci_response.length - 2 ]
+//     fibonacci_response = fibonacci_response.concat(next_number)
+
+//     return fibonacci_sequence_recursive(sequence, fibonacci_response)
+// }
+
+// console.log(fibonacci_sequence_recursive(8))
+
+const fibonacci_sequence = (sequence) => {
+        if (sequence === 0) {
+        return []
+    }
+    
     if (sequence == 1) {
         return [0]
     }
+
+    let fibonacci_response = [0, 1]
 
     if (sequence === 2) {
         return fibonacci_response;
     }
 
-    if (sequence == fibonacci_response.length) {
-        return fibonacci_response;
+    while (sequence > fibonacci_response.length) {
+        const next_number = fibonacci_response[fibonacci_response.length - 1]  + fibonacci_response[fibonacci_response.length - 2 ]
+
+        fibonacci_response = fibonacci_response.concat(next_number)
     }
 
-    const next_number = fibonacci_response[fibonacci_response.length - 1]  + fibonacci_response[fibonacci_response.length - 2 ]
-    fibonacci_response = fibonacci_response.concat(next_number)
-
-    return fibonacci_sequence(sequence, fibonacci_response)
+    return fibonacci_response
 }
 
 console.log(fibonacci_sequence(8))
