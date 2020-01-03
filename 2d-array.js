7fcd21d13502592843900c409d2fe8bd31ca38d0
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-01-03 21:41:19 +0100

    2d array

--- 2d-array.js
+++ 2d-array.js
@@ -1,17 +1,23 @@
 function hourglassSum(arr) {
-    let hourglasssumarr = [];
-    let newArr = []
-    for(i=0; i < arr.length; i++) {
-      for(j=0; j < arr.length; j++) {
-          if (j <= 3 && i <=3) {
-            let sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
-          
-           hourglasssumarr = hourglasssumarr.concat(sum)
-          
-          }
+  let hourglasssumarr = [];
+  let newArr = [];
+  for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr.length; j++) {
+      if (j <= 3 && i <= 3) {
+        let sum =
+          arr[i][j] +
+          arr[i][j + 1] +
+          arr[i][j + 2] +
+          arr[i + 1][j + 1] +
+          arr[i + 2][j] +
+          arr[i + 2][j + 1] +
+          arr[i + 2][j + 2];
+
+        hourglasssumarr = hourglasssumarr.concat(sum);
       }
     }
-    return Math.max(...hourglasssumarr)
+  }
+  return Math.max(...hourglasssumarr);
 }
 
 hourglassSum([
@@ -21,4 +27,4 @@ hourglassSum([
   [0, 0, 2, 4, 4, 0],
   [0, 0, 0, 2, 0, 0],
   [0, 0, 1, 2, 4, 0]
-])
+]);
