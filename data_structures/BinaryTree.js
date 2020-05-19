234af82df307dee4b0965e6076cb25d863321cc5
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-19 13:37:02 +0100

    add BinaryTree traverse

--- data_structures/BinaryTree.js
+++ data_structures/BinaryTree.js
@@ -1,31 +1,36 @@
 class BinaryTree {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
-  
-    traverse(tree) {
-       if (this.left) this.traverse(this.left)
-       if (this.right) this.traverse(this.right)
-    }
-
-    
-
-    insertChild(value) {
-    }
-  
-    // left, root, right
-    inOrderTraversal(func = console.log) {
-    }
-  
-    // root, left, right
-    preOrderTraversal(func = console.log) {
-    }
-  
-    // left, right, root
-    postOrderTraversal(func = console.log) {
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  export default BinaryTree;
+
+  traverse(tree) {
+    if (this.left) this.traverse(this.left);
+    if (this.right) this.traverse(this.right);
+  }
+
+  contains(value) {
+    if (this.value === value) {
+      return value;
+    }
+
+    return (
+      (this.left && this.left.contains(value) ? true : false) ||
+      (this.right && this.right.contains(value) ? true : false)
+    );
+  }
+
+  insertChild(value) {}
+
+  // left, root, right
+  inOrderTraversal(func = console.log) {}
+
+  // root, left, right
+  preOrderTraversal(func = console.log) {}
+
+  // left, right, root
+  postOrderTraversal(func = console.log) {}
+}
+
+export default BinaryTree;
